5412552fb67d9ccd048f50ff5985a880c47872de
Enzzo Cavallo <souenzzo@gmail.com>
2021-07-26 13:59:06 -0300

    WIP

---
+++ src/br/com/souenzzo/tools/npm.clj
@@ -0,0 +1,41 @@
+(ns br.com.souenzzo.tools.npm
+  (:require [clojure.data.json :as json]
+            [clojure.java.io :as io]
+            [clojure.string :as string]
+            [clojure.tools.build.api :as b])
+  (:import (java.security MessageDigest DigestInputStream)
+           (java.util Base64)
+           (java.util.zip GZIPInputStream)
+           (org.apache.commons.compress.archivers ArchiveStreamFactory)))
+
+(set! *warn-on-reflection* true)
+
+(defn install
+  [{:keys [package-lock]}]
+  (let [{:strs [packages]} (json/read (io/reader package-lock))
+        packages (remove #(get % "dev")
+                   (dissoc packages ""))]
+    (doseq [[target {:strs [resolved integrity] :as pkg}] packages
+            :let [targetf (io/file target)
+                  [alg target-b64-digest] (string/split integrity #"-" 2)
+                  md (MessageDigest/getInstance alg)
+                  in (DigestInputStream. (io/input-stream resolved) md)
+                  tar (.createArchiveInputStream (ArchiveStreamFactory.) "tar"
+                        (GZIPInputStream. in))]]
+      (loop []
+        (when-let [tar-entry (some-> tar .getNextEntry)]
+          (let [entry-target (io/file targetf (string/replace (.getName tar-entry)
+                                                #"^package/"
+                                                ""))]
+            (io/make-parents entry-target)
+            (io/copy tar entry-target)
+            (recur))))
+      (let [dig (slurp (.encode (Base64/getEncoder)
+                         (.digest md)))]
+        (when-not (= target-b64-digest dig)
+          (b/delete {:path (str targetf)})
+          (throw (ex-info (str "Dependency " resolved " do not match integrity")
+                   {:cognitect.anomalies/category :cognitect.anomalies/unavailable
+                    :expected                     integrity
+                    :actual                       integrity
+                    :pkg                          pkg})))))))
---
+++ test/br/com/souenzzo/tools/npm_test.clj
@@ -0,0 +1,7 @@
+(ns br.com.souenzzo.tools.npm-test
+  (:require [clojure.test :refer [deftest is]]))
+
+(deftest hello
+  (let []
+    (is (= 3
+          (+ 2 3)))))
